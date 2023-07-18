6f4d4d8865bb9e62185d36ed98cd639c91087fba
Maximilian Petkov <75382505+max-petkov@users.noreply.github.com>
2023-07-19 01:51:51 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
 slider({
   gap: 8,
   minScale: 0.56,
+  wheelSleep: 50, // Higher the number less slides will be scrolled (try 1000 to see difference)
+  onSelectedSlide: function(splide) {
+
+  },
+  onPaginationReady: function(splide) {
+    const pagination = document.querySelector(".pagination");
+
+    pagination.addEventListener("click", function(e) {
+      const isItem = e.target.classList.contains("pagination__item");
+
+      if(isItem) splide.go(Number(e.target.getAttribute("data-slide")));
+    });
+  }
 });
 
 function slider(config) {
   const gap = !config.gap ? 8 : config.gap;
   const minScale = !config.minScale ? 0.56 : config.minScale;
+  const startingTotalSlides = document.querySelectorAll(".splide__slide").length;
 
   generateArea();
   generateAdditionalSlides();
@@ -15,10 +29,10 @@ function slider(config) {
 
   function generateSlider() {
     const options = {
-      perPage: 9,
+      perPage: startingTotalSlides,
       perMove: 1,
-      start: 3,
-      wheelSleep: 100,
+      start: 27,
+      wheelSleep: config.wheelSleep,
       arrows: false,
       pagination: false,
       gap: gap,
@@ -31,7 +45,17 @@ function slider(config) {
       autoWidth: true,
     };
     const splide = new Splide(".splide", options);
+    splide.on("mounted", generatePagination);
     splide.mount();
+
+    if (typeof config.onSelectedSlide === "function") {
+      config.onSelectedSlide(splide);
+    }
+
+    if (typeof config.onPaginationReady === "function") {
+      config.onPaginationReady(splide);
+    }
+
   }
 
   function animate() {
@@ -66,9 +90,8 @@ function slider(config) {
       slides.forEach((slide, i) => {
         const widthSlide = Number(slide.getBoundingClientRect().width);
         const leftPositionSlide = Number(slide.getBoundingClientRect().left);
-        const insideArea =
-          widthSlide + leftPositionSlide > leftPositionArea &&
-          leftPositionSlide < rightPositionArea;
+        const insideArea = widthSlide + leftPositionSlide > leftPositionArea && leftPositionSlide < rightPositionArea;
+
         const rightPositionTrack = track.getBoundingClientRect().right;
 
         const percent = progress(Number(leftPositionSlide.toFixed(0)), -1 * widthSlide + gap, rightPositionTrack - gap).toFixed(0);
@@ -77,29 +100,30 @@ function slider(config) {
         
         if (insideArea) {
 
-          // 2️⃣ Variant of scaling and adjusting translate
-          let translateX = Math.abs(slide.children[0].getBoundingClientRect().width - slide.children[0].offsetWidth);
-
-          if(window.innerWidth > 991) {
-            slide.children[0].style.setProperty(`--translateX`, (translateX * (rightPositionTrack / 1000) * dir) + "px");
-          } else {
-            slide.children[0].style.setProperty(`--translateX`, (translateX * (rightPositionTrack / 1300) * dir) + "px");
-          }
+          const progress = progress_0_100(Number(leftPositionSlide), leftPositionArea, rightPositionArea - widthSlide).toFixed(0);
           
-         // 2️⃣ Variant of scaling and adjusting translate
-          // const progressArea = Math.round(progress_0_100(Number(leftPositionSlide), leftPositionArea, rightPositionArea - widthSlide));
+          
+          let translateX = Math.abs(slide.children[0].getBoundingClientRect().width - slide.children[0].offsetWidth);
+          if(!startingGap) translateX = (translateX * (rightPositionTrack / 750) * scale) * dir;
+          else translateX = gsap.utils.clamp(-1 * (startingGap), (startingGap), ((translateX * (rightPositionTrack / 750) * scale) * dir));
+          
+          if(progress >= 48 && progress <= 52) slide.children[0].style.setProperty(`--translateX`, 0 + "px");
+          else slide.children[0].style.setProperty(`--translateX`, translateX + "px");
+          
+          if(progress >= 35 && progress <= 65) {
+            const slideNumber = parseInt(slide.getAttribute("aria-label"));
+            const paginationItem = document.querySelectorAll(".pagination__item");
+            const position = slideNumber % startingTotalSlides;
+            const currentActivePagination = document.querySelector(".pagination__item.active");
 
-          // if(progressArea >= 25 && progressArea <= 75) {
-          //   slide.children[0].style.setProperty(`--translateX`, 0 + "px");
-          // }
+            if(currentActivePagination) currentActivePagination.classList.remove("active");
+            paginationItem[!position ? startingTotalSlides - 1 : position - 1].classList.add("active");
 
-          // if(progressArea < 25) {
-          //   slide.children[0].style.setProperty(`--translateX`, (((scale * 100) * dir) - gap * 3) + "px");
-          // }
+            slide.classList.add("active");
+          } else {
+            slide.classList.remove("active");
+          }
 
-          // if(progressArea > 75) {
-          //   slide.children[0].style.setProperty(`--translateX`, (((scale * 100) * dir) + gap * 3) + "px");
-          // }
 
           slide.classList.add("inside");
           slide.children[0].style.setProperty(`--scale`, scale);
@@ -108,7 +132,6 @@ function slider(config) {
         } else {
           // Style all Elements which are NOT inside the area
           slide.classList.remove("inside");
-          // Maybe Refactor using SWITCH statements...
 
           if(slide === lastSlide && firstSlide.classList.contains("inside")) {
             slide.children[0].style.setProperty(`--scale`, getScale(firstSlide.previousElementSibling.children[0]));
@@ -194,6 +217,7 @@ function slider(config) {
       });
     }
   }
+
   function getPositionRelativeToCenter(element) {
     const elementRect = element.getBoundingClientRect();
     const centerPosition = window.innerWidth / 2;
@@ -203,25 +227,24 @@ function slider(config) {
       return 1; // Element is to the right of the center.
     }
   }
+
   function generateArea() {
     const area = document.createElement("div");
     const slides = document.querySelectorAll(".splide__slide");
-    const slideWidth = parseInt(
-      getComputedStyle(slides[0]).getPropertyValue("--min-w")
-    );
+    const slideWidth = parseInt(getComputedStyle(slides[0]).getPropertyValue("--min-w"));
     area.style.width = slideWidth * 3 + gap * 2 + "px";
     area.classList.add("area");
     document.body.appendChild(area);
   }
+
   function getScale(el) {
     return getComputedStyle(el).getPropertyValue("--scale");
   }
+
   function getTranslateX(el) {
     return getComputedStyle(el).getPropertyValue("--translateX");
   }
-  function calculateGap(scale, minGap, maxGap) {
-    return minGap + (1 - scale) * (maxGap - minGap);
-  }
+
   function progress(value, min, max) {
     const midpoint = (min + max) / 2;
     if (value <= midpoint) {
@@ -231,23 +254,22 @@ function slider(config) {
     }
   }
 
-  function exec(cb) {
-    const start = performance.now();
-    cb();
-    const end = performance.now();
-    console.log(end - start, "ms");
-  }
-
-  function lerp(start, end, t) {
-    return start + (end - start) * t;
-  }
-  function smoothStep(t) {
-    const v1 = t * t;
-    const v2 = 1.0 - (1.0 - t) * (1.0 - t);
-    return lerp(v1, v2, t);
-  }
-
   function progress_0_100(val, min, max) {
     return ((val - min) / (max - min)) * 100;
+  }
+
+  function generatePagination() {
+    const pagination = document.createElement("div");
+    pagination.classList.add("pagination");
+    
+    for (let i = 0; i < startingTotalSlides; i++) {
+      const item = document.createElement("div");
+      item.classList.add("pagination__item");
+      item.setAttribute("data-slide", i);
+      
+      pagination.appendChild(item);
+    }
+    
+    document.querySelector("section").appendChild(pagination); // <= Parent need to be changed when moving script on Webflow
   }
 }
